3eb4954d7769b768e64974145b396dfe7bbbfb58
rydrafi13 <rafiryd13@gmail.com>
2023-05-28 09:32:30 +0700

    Update Dev

--- apps/middleware/db_connect.js
+++ apps/middleware/db_connect.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 
 // Connect database
 const connection = mysql.createConnection({
-  host: '172.17.0.2',
-  user: 'peserta',
-  password: 'password',
-  database: 'training'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 })
 
 module.exports = connection
--- apps/testing/app.test.js
+++ apps/testing/app.test.js
@@ -1,26 +1,54 @@
-const request = require('supertest')
-const express = require('express')
+const request = require('supertest');
+const mysql = require('mysql');
 
-// Import the app file
-const app = require('../app')
+const connection = require('../middleware/db_connect');
+const app = require('../app'); // Replace with the path to your application file
 
-describe('Test /', () => {
+describe('Unit Test /', () => {
   it('should respond with index.html', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
   });
 });
 
-describe('Test /app1', () => {
+describe('Unit Test /app1', () => {
     it('should respond with "Hello App1!"', async () => {
-      const response = await request(app).get('/');
+      const response = await request(app).get('/app1');
       expect(response.status).toBe(200);
     });
   });
 
-  describe('Test /app2', () => {
+  describe('Unit Test /app2', () => {
     it('should respond with "Hello App2!"', async () => {
-      const response = await request(app).get('/');
+      const response = await request(app).get('/app2');
       expect(response.status).toBe(200);
     });
   });
+
+describe('Integration Test Connect Database', () => {
+  let connection;
+
+  beforeEach(() => {
+    // Create a connection pool instead of a single connection
+    connection = mysql.createPool({
+      connectionLimit: 10, // Adjust the limit according to your requirements
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    });
+
+    // Set the connection for the application to use
+    app.set('connection', connection);
+  });
+
+  afterEach(() => {
+    // Close the connection pool after the tests are done
+    connection.end();
+  });
+
+  it('should respond with users data', async () => {
+    const response = await request(app).get('/users');
+    expect(response.status).toBe(200);
+  });
+});
