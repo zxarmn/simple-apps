3d98a832f4879de142c66c5396cc65f8e28b1120
zxarmn <zakariaramadan46@gmail.com>
2024-07-22 19:08:00 +0700

    update code

--- apps/testing/app.test.js
+++ apps/testing/app.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const mysql = require('mysql');
 require('dotenv').config();
 
-const connection = require('../middleware/db_connect');
+const connection = require('../apps/middleware/db_connect');
 const app = require('../app'); // Replace with the path to your application file
 
 describe('Unit Test /', () => {
